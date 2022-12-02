1797b8f7b6183870eb83ea8021b0b5102fb7be6d
vcdm100 <vitaomoura100@gmail.com>
2022-12-01 21:59:44 -0300

    Atualizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './assets/css/global.css';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 export function App() {
-
   return (
-    <Header />
+    
+    <>
+        <Header />
+        
+        <Footer />
+    </>
+
   )
-  
 };
