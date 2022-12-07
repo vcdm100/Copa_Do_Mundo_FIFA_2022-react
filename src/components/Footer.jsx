693f7ee9a185c1ba3fc2b5d5f7838698d230a794
vcdm100 <vitaomoura100@gmail.com>
2022-12-07 03:18:16 -0300

    Projeto "Footer" finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import LogoFifa from '../assets/images/logo-fifa/logo-fifa.svg';
 
 export function Footer() {
@@ -6,13 +6,9 @@ export function Footer() {
   return (
 
     <footer>
-
       <div className='w-full h-[24rem] flex justify-center bg-magenta-900'>
-
         <div className='flex justify-between items-center'>
-
           <div className='flex gap-32 text-gray-100'>
-
             <ul className='flex flex-col gap-2'>
               <h2 className='font-bold'>Redes Sociais</h2>
               <li>Facebook</li>
@@ -20,7 +16,6 @@ export function Footer() {
               <li>Twitter</li>
               <li>Youtube</li>
             </ul>
-
             <ul className='flex flex-col gap-2'>
               <h2 className='font-bold'>Legais</h2>
               <li>Sobre</li>
@@ -28,25 +23,17 @@ export function Footer() {
               <li>Contato</li>
               <li>Política de Privacidade</li>
             </ul>
-
             <ul className='flex flex-col gap-2'>
               <h2 className='font-bold'>Fifa</h2>
               <li>Serviços</li>
               <li>Patrocinadores</li>
             </ul>
-
           </div>
-
           <div className='w-[200px] flex flex-col items-center mx-32'>
-
             <img src={LogoFifa} />
-
           </div>
-
         </div>
-
       </div>
-
     </footer>
     
   )
