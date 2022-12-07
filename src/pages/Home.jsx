c329658b876355188a9d5d8f7922856157adf906
vcdm100 <vitaomoura100@gmail.com>
2022-12-07 03:20:17 -0300

    Projeto Pages "Home"

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { Group } from '../components/Group';
+import { Footer } from '../components/Footer';
+
+export function Home() {
+
+  return (
+
+    <div>
+        <Header />
+        <Group />
+        <Footer />
+    </div>
+
+  )
+};
